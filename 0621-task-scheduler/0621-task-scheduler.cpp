cc98c03e73972790819f8c95d1afa3d5ba875c9b
Girish_18 <128170491+Girish1845@users.noreply.github.com>
2025-05-16 22:51:51 +0530

    Time: 0 ms (100%), Space: 38.2 MB (95.29%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -5,9 +5,9 @@ public:
         vector<int> mp(26,0);
         int max_freq=0, count_maxfreq=0, sz=tasks.size();
         for(char i:tasks){
-            mp[i-'A']++;  // count the number of times a task needs to be done
+            mp[i-'A']++;  
             if(mp[i-'A']>max_freq){
-                max_freq=mp[i-'A']; // find maximum frequency 
+                max_freq=mp[i-'A']; 
             }
         }   
         for(int i=0;i<26;i++){
