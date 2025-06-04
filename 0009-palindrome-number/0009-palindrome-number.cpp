ba7a796fc484b59f269fc4db35a6baa0f141e5b2
Girish_18 <128170491+Girish1845@users.noreply.github.com>
2025-06-05 00:22:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (28.5%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+         long revNum=0;
+         int dup=x;
+        while(x>0)
+        
+        {
+            
+            int ld=x%10;
+            revNum=(revNum*10)+ld;
+
+            x=x/10;
+
+        }
+        return(revNum==dup);
+         
+        
+    }
+};
